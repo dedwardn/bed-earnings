a8197993e10d6db8abfb1397b08a57c95fb74412
dedwardn <daniel.nordas@gmail.com>
2019-08-26 22:35:24 +0200

    Updated summary and plots

--- util/clean.py
+++ util/clean.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy  as np
-from code import utils
-
-
+from util.utils import currency_to_number, boolean_to_boolean, boolean_to_int, percentage_to_ratio
 
 
 def rinse_listings(_df):
@@ -11,22 +9,41 @@ def rinse_listings(_df):
     This function takes Inside Airbnb listings
     data and cleans and removes uninteresting columns.
     For now this is hardcoded, but could be expanded to a
-    more elaborate function
+    more elaborate function. It is not rinsed of all NaNs, as we have not performed feature engineering
     :param df:
-    :return:
+    :return: cleaned dataframe
     """
     df = _df.copy(deep=True)
-    #remove columns with only 1 value
+    # remove columns with only 1 value
     df = df.drop(labels=df.nunique()[df.nunique() < 2].index, axis=1)
-    #remove columns with all unique values
-    all_unique_cols = df[df.nunique() / df.count() == 1]
-    all_unique_cols = all_unique_cols.drop(labels=['latitude', 'longitude', 'id'], axis=0).index
+    # remove columns with all unique values
+    all_unique_cols = df.loc[:,df.nunique() / df.count() == 1]
+    print(all_unique_cols.columns)
+    all_unique_cols = all_unique_cols.drop(labels=['latitude', 'longitude', 'id'], axis=1).columns
     df = df.drop(labels=all_unique_cols, axis=1)
-    #drop text columns and other non-informative columns
-    drop_columns = ['name', 'summary', 'space', 'description', 'neighborhood_overview', 'notes', 'transit', 'host_url',
-                    'host_name', 'host_location', 'host_about', 'host_thumbnail_url', 'host_picture_url',
-                    'host_neighbourhood', 'host_has_profile_pic', 'street', 'city', 'state', 'smart_location']
+    # drop text columns and other non-informative columns
+    drop_columns = ['square_feet', 'name', 'summary', 'space', 'description', 'neighborhood_overview', 'notes',
+                    'transit', 'host_url', 'host_name', 'host_location', 'host_about', 'host_thumbnail_url',
+                    'host_picture_url', 'host_neighbourhood', 'host_has_profile_pic', 'street', 'city', 'state',
+                    'smart_location']
     df = df.drop(drop_columns, axis=1)
-
+    # reformat currency to number, assuming dollar
+    df['monthly_price'] = df['monthly_price'].map(currency_to_number)
+    df['price'] = df['price'].map(currency_to_number)
+    df['weekly_price'] = df['weekly_price'].map(currency_to_number)
+    df['security_deposit'] = df['security_deposit'].map(currency_to_number)
+    df['cleaning_fee'] = df['cleaning_fee'].map(currency_to_number)
+    df['extra_people'] = df['extra_people'].map(currency_to_number)
+    # reformat percentage to ratio
+    df['host_response_rate'] = df['host_response_rate'].map(percentage_to_ratio)
+    df['host_acceptance_rate'] = df['host_acceptance_rate'].map(percentage_to_ratio)
+    # reformat boolean values to pythonic booleans or ints
+    df['instant_bookable'] = df['instant_bookable'].map(boolean_to_boolean)
+    df['require_guest_profile_picture'] = df['require_guest_profile_picture'].map(boolean_to_boolean)
+    df['require_guest_phone_verification'] = df['require_guest_phone_verification'].map(boolean_to_boolean)
+    df['is_location_exact'] = df['is_location_exact'].map(boolean_to_boolean)
+    df['host_is_superhost'] = df['host_is_superhost'].map(boolean_to_int)
+    df['host_identity_verified'] = df['host_identity_verified'].map(boolean_to_boolean)
+    return df
 
 
--- util/utils.py
+++ util/utils.py
@@ -1,6 +1,11 @@
+import os
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 import numpy as np
 import pandas as pd
 import math
+import folium
+
 
 
 def currency_to_number(x):
@@ -71,3 +76,70 @@ def boolean_to_int(x):
     else:
         print(x)
         raise TypeError
+
+
+def regplot(x, y, label, color, **kwargs):
+    plt.scatter(x, y, c='k')
+    name = x.name
+    x_reg = sm.add_constant(pd.Series(data=np.linspace(np.min(x), np.max(x), num=20), name=x.name))
+    x = sm.add_constant(x)
+    mod = sm.OLS(y, x, has_constant='add')
+    res = mod.fit()
+    plt.plot(x_reg[name], res.predict(x_reg), 'r')
+    plt.annotate("R2: {:.3}".format(res.rsquared), xy=(0.9, 1), xycoords='axes fraction')
+    plt.annotate("y={:.3}x + {:.3}".format(res.params[name], res.params['const']), xy=(0.01, 1),
+                 xycoords='axes fraction')
+    if name == 'price':
+        if y.name == 'weekly_price':
+            plt.plot(x_reg[name], 7 * x_reg[name], label='7xprice')
+        if y.name == 'monthly_price':
+            plt.plot(x_reg[name], 30 * x_reg[name], label='30xprice')
+    if name == 'weekly_price' and y.name == 'monthly_price':
+        plt.plot(x_reg[name], 4 * x_reg[name], label='4xweekly price')
+    plt.legend()
+    return res
+
+
+def _inline_map(m, width=650, height=500):
+    from IPython.display import HTML, display
+    srcdoc = m.HTML.replace('"', '&quot;')
+    embed = HTML('<iframe srcdoc="{}" '
+                 'style="width :{}px; height: {}px; display:block; width: 50%; margin: 0 auto;'
+                 'border: none"></iframe>'.format(srcdoc, width, height))
+    return embed
+
+
+def plot_map(df, loc, data_col, prefix=None, bins=None):
+    neighborhoods_json = os.path.join("seattle_neighborhoods.geojson")
+    m = folium.Map(location=[47.611406, -122.337498], zoom_start=12)
+
+    if loc == 'neighbourhood' or loc == 'neighbourhood_cleansed':
+        keyon = 'feature.properties.name'
+    elif loc == 'neighbourhood_group_cleansed':
+        keyon = 'feature.properties.nhood'
+
+    else:
+        return None
+
+    if bins is None:
+        bins = 6
+
+    folium.Choropleth(
+        geo_data=neighborhoods_json,
+        name='choropleth',
+        data=df,
+        columns=[loc, data_col],
+        key_on=keyon,
+        fill_color='YlGnBu',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name=data_col,
+        reset=True,
+        bins=bins
+    ).add_to(m)
+    folium.LayerControl().add_to(m)
+    if prefix:
+        m.save('{}_{}.html'.format(prefix, data_col))
+    else:
+        m.save('{}.html'.format(data_col))
+    return m
